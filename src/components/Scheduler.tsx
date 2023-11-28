101ad1c2199ae1cf5c141ffc85c5321ac4fc5623
Patrick the Ratrick <keavenyp@gmail.com>
2023-11-28 12:53:58 -0700

    Removed react-big-calendar and replaced it with FullCalendar
    - react-big-calendar didn't work and required a lot more added dependencies than expected
    - trying out FullCalendar to see if it works better

--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -10,43 +10,10 @@ const localizer = momentLocalizer(moment)
 function Scheduler() {
     // const [ events, setEvents ] = useState<any[]>([])
 
-    const [myEvents, setEvents] = useState([{}])
-
-    const handleSelectSlot = useCallback(
-      ({ start, end }: {start: any, end: any}) => {
-        const title = window.prompt('New Event name')
-        if (title) {
-          setEvents((prev) => [...prev, { start, end, title }])
-        }
-      },
-      [setEvents]
-    )
-  
-    const handleSelectEvent = useCallback(
-      (event: any) => window.alert(event.title),
-      []
-    )
-  
-    const { defaultDate, scrollToTime } = useMemo(
-      () => ({
-        defaultDate: new Date(2015, 3, 12),
-        scrollToTime: new Date(1970, 1, 1, 6),
-      }),
-      []
-    )
+   
     return (
       <div className="scheduler">
-        <Calendar
-            defaultDate={defaultDate}
-            defaultView={Views.WEEK}
-            events={myEvents}
-            localizer={localizer}
-            longPressThreshold={10}
-            onSelectEvent={handleSelectEvent}
-            onSelectSlot={handleSelectSlot}
-            selectable
-            scrollToTime={scrollToTime}
-            />
+
       </div>
     );
   }
